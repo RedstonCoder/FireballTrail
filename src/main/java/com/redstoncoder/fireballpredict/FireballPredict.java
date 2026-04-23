6ade18fea8cdfa861be8a076a64f8f5ad681dff2
RedstonCoder <205503404+RedstonCoder@users.noreply.github.com>
2026-04-24 05:34:43 +0800

    1.0.2a

--- src/main/java/com/redstoncoder/fireballpredict/FireballPredict.java
+++ src/main/java/com/redstoncoder/fireballpredict/FireballPredict.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.relauncher.Side;
 )
 public class FireballPredict {
     public static final String MODID = "fireballpredict";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.2a";
     public static final String NAME = "FireballPredict";
 
     @EventHandler
